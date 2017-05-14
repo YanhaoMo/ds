3ac725c1a6b9dcd6216b594c492f308ff7cd27db
YanhaoMo <moyanhao@outlook.com>
2017-05-14 12:58:01 +0800

    stack finished

--- stack.c
+++ stack.c
@@ -1,4 +1,5 @@
 #include <malloc.h>
+#include <string.h>
 
 #define TYPE int
 
@@ -18,6 +19,8 @@ stack* create(long cap) {
     st->data = data;
     st->cap = cap;
     st->lenght = 0;
+    st->top = &(st->data[cap-1]);
+    st->bottom = st->top - 1;
 
     return st;
 }
@@ -29,17 +32,36 @@ int destroy(stack* st) {
 }
 
 TYPE* pop(stack* st) {
-
+    if (st->lenght == 0) {
+        return NULL;
+    } else {
+        TYPE* temp = (TYPE*)malloc(sizeof(TYPE));
+        memcpy(st->top, temp, sizeof(TYPE));
+        st->top --;
+        st->lenght --;
+        return temp;
+    }
 }
 
 int push(stack* st, TYPE s) {
-
+    if (st->lenght < st->cap) {
+        st->top ++;
+        *(st->top) = s;
+        st->lenght ++;
+        return 0;
+    } else {
+        return -1;
+    }
 }
 
 long length(stack* st) {
-
+    return st->lenght;
 }
 
 int isempty(stack* st) {
-
+    if (st->lenght == st->cap) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
